1b4cdbf16da92d44369aa6ea2ef9b09859653bd6
vermamehak528-alt <vermamehak528@gmail.com>
2026-08-01 19:46:03 +0530

    Add Firebase configuration and authentication setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Firebase Configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsJ8BIXu3xvaMnCwF8WNesRPJfy9zbadg",
+  authDomain: "profit-ai-37155.firebaseapp.com",
+  projectId: "profit-ai-37155",
+  storageBucket: "profit-ai-37155.firebasestorage.app",
+  messagingSenderId: "245284035512",
+  appId: "1:245284035512:web:027d79d90a9130536d0d4d"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
