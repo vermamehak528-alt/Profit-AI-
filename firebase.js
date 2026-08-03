0f1e0b3304256eb6d9770962df5b164f5f6f899a
vermamehak528-alt <vermamehak528@gmail.com>
2026-08-03 19:29:59 +0530

    Refactor Firebase initialization and exports
    Removed firebaseConfig object and reorganized exports.

--- firebase.js
+++ firebase.js
@@ -2,16 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCsJ8BIXu3xvaMnCwF8WNesRPJfy9zbadg",
-  authDomain: "profit-ai-37155.firebaseapp.com",
-  projectId: "profit-ai-37155",
-  storageBucket: "profit-ai-37155.firebasestorage.app",
-  messagingSenderId: "245284035512",
-  appId: "1:245284035512:web:00c72b557fc04bf16d0d4d"
-};
-
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
