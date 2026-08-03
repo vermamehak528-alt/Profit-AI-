4d7c3c28a22320ecb432eeecbd7e07aeb689d88c
vermamehak528-alt <vermamehak528@gmail.com>
2026-08-03 18:56:09 +0530

    Update Firebase configuration and export statements

--- firebase.js
+++ firebase.js
@@ -3,17 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCsJ8BIXu3xvaMnCwF8WNesRPJfy9zbadg",
   authDomain: "profit-ai-37155.firebaseapp.com",
   projectId: "profit-ai-37155",
   storageBucket: "profit-ai-37155.firebasestorage.app",
   messagingSenderId: "245284035512",
-  appId: "1:245284035512:web:027d79d90a9130536d0d4d"
+  appId: "1:245284035512:web:00c72b557fc04bf16d0d4d"
 };
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
