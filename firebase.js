c4073636964c01238af8992fbf9e601f0f4a3835
vermamehak528-alt <vermamehak528@gmail.com>
2026-08-03 20:02:52 +0530

    Add Firebase configuration and initialization
    Added Firebase configuration for app initialization.

--- firebase.js
+++ firebase.js
@@ -2,6 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCsJ8BIXu3xvaMnCwF8WNesRPJfy9zbadg",
+  authDomain: "profit-ai-37155.firebaseapp.com",
+  projectId: "profit-ai-37155",
+  storageBucket: "profit-ai-37155.firebasestorage.app",
+  messagingSenderId: "245284035512",
+  appId: "1:245284035512:web:00c72b557fc04bf16d0d4d"
+};
+
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
