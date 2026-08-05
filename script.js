2a9e193a87399dbcedb7c5dc499833d53db24b5d
vermamehak528-alt <vermamehak528@gmail.com>
2026-08-05 17:20:25 +0530

    Refactor send function and integrate Firebase

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 
-const API_KEY = "AQ.Ab8RN6JNEvg3TuV3o_TuR0byVgotWrnhAl-AIOBH2OpiZh_Vqg";
+import { auth, db } from "./firebase.js";
 
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  collection,
+  addDoc,
+  getDocs,
+  orderBy,
+  query,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+let currentUser = null;
+
+const API_KEY = "AQ.Ab8RN6JEj7mhtJ_FXI4PujH0worPIdYz9Anicm0j5tvgWJ-zFA";
+
+const chat = document.getElementById("chat");
+const input = document.getElementById("msg");
+
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+    currentUser = user;
+    loadMessages();
+});
+async function loadMessages() {
+
+    chat.innerHTML = "";
+
+    const q = query(
+        collection(db, "users", currentUser.uid, "chats"),
+        orderBy("time")
+    );
+
+    const snapshot = await getDocs(q);
+
+    snapshot.forEach((doc) => {
+
+        const data = doc.data();
+
+        chat.innerHTML += `
+        <div class="${data.role}">
+            ${data.text}
+        </div>
+        `;
+
+    });
+
+    chat.scrollTop = chat.scrollHeight;
+}
 async function send() {
   const input = document.getElementById("msg");
   const chat = document.getElementById("chat");
@@ -9,9 +63,25 @@ async function send() {
   if (!message) return;
 
   chat.innerHTML += `<div class="user">${message}</div>`;
+  await addDoc(
+    collection(db, "users", currentUser.uid, "chats"),
+    {
+        role: "user",
+        text: message,
+        time: serverTimestamp()
+    }
+);
   input.value = "";
 
   chat.innerHTML += `<div class="bot" id="loading">Thinking...</div>`;
+  await addDoc(
+    collection(db, "users", currentUser.uid, "chats"),
+    {
+        role: "bot",
+        text: reply,
+        time: serverTimestamp()
+    }
+);
   chat.scrollTop = chat.scrollHeight;
 
   try {
@@ -56,8 +126,22 @@ async function send() {
     chat.scrollTop = chat.scrollHeight;
 
   } catch (error) {
-    document.getElementById("loading").remove();
-    chat.innerHTML += `<div class="bot">Error connecting to Gemini API.</div>`;
+
+    const loading = document.getElementById("loading");
+    if (loading) loading.remove();
+
+    console.error(error);
+
+    chat.innerHTML += `
+        <div class="bot">
+            ⚠️ Error: Unable to connect to Profit AI.
+            Please try again.
+        </div>
+    `;
+
+    chat.scrollTop = chat.scrollHeight;
   }
-}
+  }
+
 window.send = send;
+chat.scrollTop = chat.scrollHeight;
