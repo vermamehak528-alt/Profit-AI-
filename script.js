cf04155fa8ca98ad2b73e0347b922485e24cff01
vermamehak528-alt <vermamehak528@gmail.com>
2026-08-07 17:54:01 +0530

    Update API call to use Groq API

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "AQ.Ab8RN6LyBRMXJjzsXUSSnHOLj0LKEvbNOdKEaAhS9QN6_WpqAgom";
+const API_KEY = "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";
 
 async function send() {
   const input = document.getElementById("msg");
@@ -14,45 +14,35 @@ async function send() {
   chat.scrollTop = chat.scrollHeight;
 
   try {
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          contents: [
-            {
-              parts: [
-                {
-                  text: message
-                }
-              ]
-            }
-          ]
-        })
-      }
-    );
+    const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
+  method: "POST",
+  headers: {
+    "Authorization": `Bearer ${API_KEY}`,
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+  model: "openai/gpt-oss-20b",
+  messages: [
+    {
+      role: "user",
+      content: message
+    }
+  ]
+})
+});
 
     const data = await response.json();
-    console.log(response.status);
-    console.log(data);
-    document.getElementById("loading").remove();
 
-    if (!response.ok) {
-  alert(JSON.stringify(data, null, 2));
-    }
+let reply = "Sorry, I couldn't generate a response.";
 
-    if (
-      data.candidates &&
-      data.candidates.length > 0 &&
-      data.candidates[0].content &&
-      data.candidates[0].content.parts &&
-      data.candidates[0].content.parts.length > 0
-    ) {
-      reply = data.candidates[0].content.parts[0].text;
-    }
+if (
+  data.choices &&
+  data.choices.length > 0 &&
+  data.choices[0].message
+) {
+  reply = data.choices[0].message.content;
+}
+  
 
     chat.innerHTML += `<div class="bot">${reply}</div>`;
     chat.scrollTop = chat.scrollHeight;
